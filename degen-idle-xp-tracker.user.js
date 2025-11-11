46b8a9cd569a291c9d91b4d4d55581ac8fed8982
Seisen99 <lpfrancisco99@gmail.com>
2025-11-11 11:11:39 +0000

    Test webhook again - bump version to 1.0.2

--- degen-idle-xp-tracker.user.js
+++ degen-idle-xp-tracker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Degen Idle XP Tracker
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Track XP progression and calculate time to next levels
 // @author       Seisen99
 // @license      MIT
