ee9f19b6ce1b2ea905bd65e32c53bb98983f558e
Seisen99 <lpfrancisco99@gmail.com>
2025-11-12 19:11:59 +0000

    Update CDN hashes to v3.0.11 (2efbfff)

--- v3/degen-idle-xp-tracker-v3.user.js
+++ v3/degen-idle-xp-tracker-v3.user.js
@@ -7,14 +7,14 @@
 // @match        https://degenidle.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=degenidle.com
 // @grant        none
-// @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@TEMP_HASH/v3/modules/constants.js
+// @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@2efbfff/v3/modules/constants.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@765009b/v3/modules/game-database.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@5e9c413/v3/modules/database-loader.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@5e9c413/v3/modules/efficiency-calculator.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@5e9c413/v3/modules/item-data-engine.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@5e9c413/v3/modules/api-handler.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@5e9c413/v3/modules/state-manager-enhanced.js
-// @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@TEMP_HASH/v3/modules/ui-manager-enhanced.js
+// @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@2efbfff/v3/modules/ui-manager-enhanced.js
 // @require      https://cdn.jsdelivr.net/gh/Seisen99/Degen-Idle-XP-Tracker@5e9c413/v3/modules/optimizer.js
 // ==/UserScript==
 
